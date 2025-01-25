5954c74330bf93f8cc1d7abffe83fb7cad2b6e11
alisa-lu <alisasl@uci.edu>
2025-01-25 15:10:19 -0800

    Update user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,23 +2,27 @@ class User {
   final String uid;
   String name;
   String email;
+  int points;
 
   User({
     required this.uid,
     this.name = '',
     this.email = '',
+    this.points = 0,
   });
 
   User.fromData(Map<String, dynamic> data)
       : uid = data['uid'],
         name = data['name'] ?? '',
-        email = data['email'] ?? '';
+        email = data['email'] ?? '',
+        points = data['points'] ?? 0;
 
   Map<String, dynamic> toJson() {
     return {
       'uid': uid,
       'name': name,
       'email': email,
+      'points': points,
     };
   }
 }
