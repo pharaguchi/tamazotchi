dbd145f46ddd13c28907277b66c3c6642f18fcee
Sharon <sharon.m0626@gmail.com>
2025-01-25 13:55:18 -0800

    fix: change path for home in wrapper

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,7 +1,7 @@
 import 'package:tamazotchi/components/loading.dart';
 import 'package:tamazotchi/models/user.dart';
 import 'package:tamazotchi/screens/authenticate/authenticate.dart';
-import 'package:tamazotchi/screens/home/home.dart';
+import 'package:tamazotchi/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tamazotchi/services/database.dart';
