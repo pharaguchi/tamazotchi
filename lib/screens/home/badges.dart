24b7cd288305d995dbc156e37ae671ac19fd5cf0
Sharon <sharon.m0626@gmail.com>
2025-01-26 06:22:52 -0800

    feat: sync badge progress with db stats

--- lib/screens/home/badges.dart
+++ lib/screens/home/badges.dart
@@ -1,81 +1,184 @@
 import 'package:flutter/material.dart';
 import 'package:tamazotchi/screens/home/badgesDetail.dart';
+import 'package:tamazotchi/models/user.dart';
+import 'package:tamazotchi/services/database.dart';
 
 class BadgePage extends StatefulWidget {
+  late User _user;
+  late final badges;
+
+  BadgePage({Key? key, required User user})
+      : _user = user,
+        super(key: key);
+
   @override
   _BadgePageState createState() => _BadgePageState();
 }
 
 class _BadgePageState extends State<BadgePage> {
-  // List of badges with milestone info and image paths
-  final List<Map<String, dynamic>> badges = [
-    {
-      'title': 'Sustainable Transportation',
-      'questionImage': 'assets/Badges/question.jpg',  // Shared question mark image
-      'sketchImage': 'assets/Badges/athlete_sketch.jpg',  // Specific sketch image
-      'fullImage': 'assets/Badges/athlete.jpeg',  // Specific full badge image
-      'description': 'Progress Check:',
-      'milestones': [
-        {'description': '5-mile distance', 'goal': 5, 'progress': 5},
-        {'description': '10-mile distance', 'goal': 10, 'progress': 10},
-        {'description': '50-mile distance', 'goal': 50, 'progress': 50},
-      ]
-    },
-    {
-      'title': 'Community Engagement',
-      'questionImage': 'assets/Badges/question.jpg',  // Shared question mark image
-      'sketchImage': 'assets/Badges/flower_sketch.jpg',  // Specific sketch image
-      'fullImage': 'assets/Badges/flower.jpeg',  // Specific full badge image
-      'description': 'Progress Check:',
-      'milestones': [
-        {'description': '5 hours', 'goal': 5, 'progress': 5},
-        {'description': '10 hours', 'goal': 10, 'progress': 10},
-        {'description': '50 hours', 'goal': 50, 'progress': 50},
-      ]
-    },
-    {
-      'title': 'Conscious Conservation',
-      'questionImage': 'assets/Badges/question.jpg',  // Shared question mark image
-      'sketchImage': 'assets/Badges/sun_sketch.jpg',  // Specific sketch image
-      'fullImage': 'assets/Badges/sun.jpeg',  // Specific full badge image
-      'description': 'Progress Check:',
-      'milestones': [
-        {'description': '5 eco-friendly items', 'goal': 5, 'progress': 5},
-        {'description': '10 eco-friendly items', 'goal': 10, 'progress': 10},
-        {'description': '50 eco-friendly items', 'goal': 50, 'progress': 20},
-      ]
-    },
-    {
-      'title': 'Reusable Waterbottle',
-      'questionImage': 'assets/Badges/question.jpg',  // Shared question mark image
-      'sketchImage': 'assets/Badges/water_sketch.jpg',  // Specific sketch image
-      'fullImage': 'assets/Badges/water.jpeg',  // Specific full badge image
-      'description': 'Progress Check:',
-      'milestones': [
-        {'description': '64 ounces', 'goal': 64, 'progress': 5},
-        {'description': '128 ounces', 'goal': 128, 'progress': 10},
-        {'description': '265 ounces', 'goal': 256, 'progress': 50},
-      ]
-    },
-    {
-      'title': 'Waste Reduction',
-      'questionImage': 'assets/Badges/question.jpg',
-      'sketchImage': 'assets/Badges/trashcan_sketch.jpg',
-      'fullImage': 'assets/Badges/trashcan.jpeg',
-      'description': 'Progress Check:',
-      'milestones': [
-        {'description': '5 items recycled or composed', 'goal': 5, 'progress': 5},
-        {'description': '10 items recycled or compose', 'goal': 10, 'progress': 10},
-        {'description': '50 items recycled or compose', 'goal': 50, 'progress': 50},
-      ]
-    },
-    // Add other badges with specific images...
-  ];
+  late User _user;
+  late final DatabaseService databaseService;
+  late List<Map<String, dynamic>> badges;
+
+  @override
+  void initState() {
+    _user = widget._user;
+    databaseService = DatabaseService(
+      uid: _user.uid,
+    );
+
+    // List of badges with milestone info and image paths
+    badges = [
+      {
+        'title': 'Sustainable Transportation',
+        'questionImage':
+            'assets/Badges/question.jpg', // Shared question mark image
+        'sketchImage':
+            'assets/Badges/athlete_sketch.jpg', // Specific sketch image
+        'fullImage': 'assets/Badges/athlete.jpeg', // Specific full badge image
+        'description': 'Progress Check:',
+        'milestones': [
+          {
+            'description': '5-mile distance',
+            'goal': 5,
+            'progress': _user.miles > 5 ? 5 : _user.miles
+          },
+          {
+            'description': '10-mile distance',
+            'goal': 10,
+            'progress': _user.miles > 10 ? 10 : _user.miles
+          },
+          {
+            'description': '50-mile distance',
+            'goal': 50,
+            'progress': _user.miles > 50 ? 50 : _user.miles
+          },
+        ]
+      },
+      {
+        'title': 'Community Engagement',
+        'questionImage':
+            'assets/Badges/question.jpg', // Shared question mark image
+        'sketchImage':
+            'assets/Badges/flower_sketch.jpg', // Specific sketch image
+        'fullImage': 'assets/Badges/flower.jpeg', // Specific full badge image
+        'description': 'Progress Check:',
+        'milestones': [
+          {
+            'description': '5 hours',
+            'goal': 5,
+            'progress': _user.hoursVolunteered > 5 ? 5 : _user.hoursVolunteered
+          },
+          {
+            'description': '10 hours',
+            'goal': 10,
+            'progress':
+                _user.hoursVolunteered > 10 ? 10 : _user.hoursVolunteered
+          },
+          {
+            'description': '50 hours',
+            'goal': 50,
+            'progress':
+                _user.hoursVolunteered > 50 ? 50 : _user.hoursVolunteered
+          },
+        ]
+      },
+      {
+        'title': 'Conscious Conservation',
+        'questionImage':
+            'assets/Badges/question.jpg', // Shared question mark image
+        'sketchImage': 'assets/Badges/sun_sketch.jpg', // Specific sketch image
+        'fullImage': 'assets/Badges/sun.jpeg', // Specific full badge image
+        'description': 'Progress Check:',
+        'milestones': [
+          {
+            'description': '5 eco-friendly items',
+            'goal': 5,
+            'progress':
+                _user.energyCostSavings > 5 ? 5 : _user.energyCostSavings
+          },
+          {
+            'description': '10 eco-friendly items',
+            'goal': 10,
+            'progress':
+                _user.energyCostSavings > 10 ? 10 : _user.energyCostSavings
+          },
+          {
+            'description': '50 eco-friendly items',
+            'goal': 50,
+            'progress':
+                _user.energyCostSavings > 50 ? 50 : _user.energyCostSavings
+          },
+        ]
+      },
+      {
+        'title': 'Reusable Waterbottle',
+        'questionImage':
+            'assets/Badges/question.jpg', // Shared question mark image
+        'sketchImage':
+            'assets/Badges/water_sketch.jpg', // Specific sketch image
+        'fullImage': 'assets/Badges/water.jpeg', // Specific full badge image
+        'description': 'Progress Check:',
+        'milestones': [
+          {
+            'description': '64 ounces',
+            'goal': 64,
+            'progress': _user.ounces > 64 ? 64 : _user.ounces
+          },
+          {
+            'description': '128 ounces',
+            'goal': 128,
+            'progress': _user.ounces > 128 ? 128 : _user.ounces
+          },
+          {
+            'description': '265 ounces',
+            'goal': 256,
+            'progress': _user.ounces > 128 ? 128 : _user.ounces
+          },
+        ]
+      },
+      {
+        'title': 'Waste Reduction',
+        'questionImage': 'assets/Badges/question.jpg',
+        'sketchImage': 'assets/Badges/trashcan_sketch.jpg',
+        'fullImage': 'assets/Badges/trashcan.jpeg',
+        'description': 'Progress Check:',
+        'milestones': [
+          {
+            'description': '5 items recycled or composed',
+            'goal': 5,
+            'progress': _user.itemsRecycledOrComposted > 5
+                ? 5
+                : _user.itemsRecycledOrComposted
+          },
+          {
+            'description': '10 items recycled or compose',
+            'goal': 10,
+            'progress': _user.itemsRecycledOrComposted > 10
+                ? 10
+                : _user.itemsRecycledOrComposted
+          },
+          {
+            'description': '50 items recycled or compose',
+            'goal': 50,
+            'progress': _user.itemsRecycledOrComposted > 50
+                ? 50
+                : _user.itemsRecycledOrComposted
+          },
+        ]
+      },
+      // Add other badges with specific images...
+    ];
+
+    super.initState();
+  }
 
   // Helper function to determine the correct image based on milestone progress
   String _getBadgeImage(int index) {
     List<dynamic> milestones = badges[index]['milestones'];
-    int completedMilestones = milestones.where((milestone) => milestone['progress'] >= milestone['goal']).length;
+    int completedMilestones = milestones
+        .where((milestone) => milestone['progress'] >= milestone['goal'])
+        .length;
 
     if (completedMilestones == 0) {
       // No milestones completed: Question mark image (shared image)
@@ -94,7 +197,8 @@ class _BadgePageState extends State<BadgePage> {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => BadgeDetailsPage(badgeIndex: badgeIndex, badges: badges),
+        builder: (context) =>
+            BadgeDetailsPage(badgeIndex: badgeIndex, badges: badges),
       ),
     );
   }
@@ -107,9 +211,9 @@ class _BadgePageState extends State<BadgePage> {
         padding: const EdgeInsets.all(16.0),
         child: GridView.builder(
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,  // 2 columns
-            crossAxisSpacing: 16,  // Space between columns
-            mainAxisSpacing: 16,   // Space between rows
+            crossAxisCount: 2, // 2 columns
+            crossAxisSpacing: 16, // Space between columns
+            mainAxisSpacing: 16, // Space between rows
           ),
           itemCount: badges.length,
           itemBuilder: (context, index) {
@@ -119,10 +223,13 @@ class _BadgePageState extends State<BadgePage> {
             return GestureDetector(
               onTap: () => _navigateToBadgeDetails(context, index),
               child: Column(
-                mainAxisSize: MainAxisSize.min,  // Let the column take the minimum space it needs
+                mainAxisSize:
+                    MainAxisSize.min, // Let the column take the minimum space
                 children: [
                   // Use the dynamically chosen image
-                  Image.asset(badgeImage, height: 200, width: 200),
+                  Flexible(
+                    child: Image.asset(badgeImage, fit: BoxFit.cover),
+                  ),
                   SizedBox(height: 8),
                   Text(
                     badges[index]['title'],
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -33,12 +33,16 @@ class _HomeScreenState extends State<HomeScreen> {
   // Method to navigate to the BadgePage
   void _navigateToBadges() {
     // Optionally update the navigation bar index (if needed)
-    setNavBarIdx(1); // You can adjust the index to the BadgePage index in your navigation bar
+    setNavBarIdx(
+        1); // You can adjust the index to the BadgePage index in your navigation bar
 
     // Navigate to the BadgePage
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => BadgePage()), // Ensure BadgePage exists or is imported
+      MaterialPageRoute(
+          builder: (context) => BadgePage(
+                user: _user,
+              )), // Ensure BadgePage exists or is imported
     );
   }
 
@@ -47,75 +51,86 @@ class _HomeScreenState extends State<HomeScreen> {
     return Column(
       children: [
         // The rest of the content in the column, centered
-        SizedBox(height: 60), // Add some space to push the rest of the content down
+        SizedBox(
+            height: 60), // Add some space to push the rest of the content down
         Center(
           child: Text(
-            'Hi ${_user.name}! I missed you <3',  // Main text/content
+            'Hi ${_user.name}! I missed you <3', // Main text/content
             style: TextStyle(fontSize: 24),
           ),
         ),
         // Align the entire widget (icon + text) to the top-right corner
-        Row (
+        Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             GestureDetector(
-            onTap: _navigateToBadges,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 16, left: 16),  // Padding from top and right
-              child: Column(
-                mainAxisSize: MainAxisSize.min,  // The column only takes as much space as needed
-                crossAxisAlignment: CrossAxisAlignment.center,  // Center icon and text horizontally
-                children: [
-                  Icon(
-                    Icons.chat_outlined,
-                    size: 70,  // Size of the badge icon
-                    color: Colors.blue,  // Icon color
-                  ),
-                  SizedBox(height: 8),  // Space between the icon and the text
-                  Text(
-                    'My Posts',  // Text displayed under the icon
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
+              onTap: _navigateToBadges,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    top: 16, left: 16), // Padding from top and right
+                child: Column(
+                  mainAxisSize: MainAxisSize
+                      .min, // The column only takes as much space as needed
+                  crossAxisAlignment: CrossAxisAlignment
+                      .center, // Center icon and text horizontally
+                  children: [
+                    Icon(
+                      Icons.chat_outlined,
+                      size: 70, // Size of the badge icon
+                      color: Colors.blue, // Icon color
+                    ),
+                    SizedBox(height: 8), // Space between the icon and the text
+                    Text(
+                      'My Posts', // Text displayed under the icon
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          GestureDetector(
-            onTap: _navigateToBadges,  // Navigate to BadgePage on tap
-            child: Padding(
-              padding: const EdgeInsets.only(top: 16, right: 16),  // Padding from top and right
-              child: Column(
-                mainAxisSize: MainAxisSize.min,  // The column only takes as much space as needed
-                crossAxisAlignment: CrossAxisAlignment.center,  // Center icon and text horizontally
-                children: [
-                  Icon(
-                    Icons.badge_outlined,
-                    size: 70,  // Size of the badge icon
-                    color: Colors.blue,  // Icon color
-                  ),
-                  SizedBox(height: 8),  // Space between the icon and the text
-                  Text(
-                    'My Badges',  // Text displayed under the icon
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
+            GestureDetector(
+              onTap: _navigateToBadges, // Navigate to BadgePage on tap
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    top: 16, right: 16), // Padding from top and right
+                child: Column(
+                  mainAxisSize: MainAxisSize
+                      .min, // The column only takes as much space as needed
+                  crossAxisAlignment: CrossAxisAlignment
+                      .center, // Center icon and text horizontally
+                  children: [
+                    Icon(
+                      Icons.badge_outlined,
+                      size: 70, // Size of the badge icon
+                      color: Colors.blue, // Icon color
+                    ),
+                    SizedBox(height: 8), // Space between the icon and the text
+                    Text(
+                      'My Badges', // Text displayed under the icon
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
           ],
         ),
-        SizedBox(height: MediaQuery.of(context).size.width * 0.1,),
-        Image (
-          image: AssetImage('assets/${getTamagotchiImageLink(_user.tamagotchi)}'),
+        SizedBox(
+          height: MediaQuery.of(context).size.width * 0.1,
+        ),
+        Image(
+          image:
+              AssetImage('assets/${getTamagotchiImageLink(_user.tamagotchi)}'),
           fit: BoxFit.contain,
           width: MediaQuery.of(context).size.width * 0.6,
           height: MediaQuery.of(context).size.width * 0.7,
         ),
-        SizedBox(height: MediaQuery.of(context).size.width * 0.1,),
+        SizedBox(
+          height: MediaQuery.of(context).size.width * 0.1,
+        ),
       ],
     );
-}
-
+  }
 }
 
 //   @override
